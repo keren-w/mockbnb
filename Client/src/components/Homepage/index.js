90f450c6d2294de284320b167c8c0a451bab96a9
keren <keren.w@gmail.com>
2018-01-03 12:23:25 +0200

    gitignore

--- Client/src/components/Homepage/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import LocationsIndex from '../LocationsIndex';
-
-// This tiny component exists so we can modify which components we want in the homepage anytime.
-export default () => <LocationsIndex />
--- Server/constants.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    security: {
-    sessionSecret: "jflsdjnfklmldeihwowh",
-    sessionExpiration: '12H'
-    },
-    mongo: {
-        uri: "mongodb://localhost/mockbnb"    
-    }
-}
